46fbbc6705f1c91fbafd503770960b7354206617
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2023-09-07 14:07:49 +0000

    Made heading center aligned

--- src/components/allVideos/VideoContainer.jsx
+++ src/components/allVideos/VideoContainer.jsx
@@ -4,11 +4,17 @@ import VideoCard from "./VideoCard";
 const VideoContainer = ({
     _allVideos
 }) =>{
-    return <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center gap-5 p-2 ">
+    return <section>
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center gap-5 p-2 ">
         {
-            _allVideos.length > 0 ? _allVideos.map((videos,i)=>{
+            _allVideos.length > 0 && _allVideos.map((videos,i)=>{
                 return <VideoCard key={i} _videoSrc={videos.video} _heading={videos.heading} _tags={videos.tags} />
-            }) : <h1 className="text-center justify-self-center">No Videos</h1>
+            })
+        }
+        </section>
+
+        {
+            _allVideos.length <= 0 && <h1 className="text-center text-3xl">No Videos</h1>
         }
     </section>
 }
--- src/components/ui/inputs/SearchBar.jsx
+++ src/components/ui/inputs/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBar = ({
     _onChange
 }) =>{
-    return <input onChange={_onChange} className="input input-warning p-1 max-w-[20rem]" placeholder="Search" />
+    return <input onChange={_onChange} className="input input-warning p-1 max-w-[20rem]" placeholder="Search Video" />
 }
 
 export default SearchBar
